aff0827f358ee30e2d57acce96dfaa232a248309
Sarah Turek <snturek@gmail.com>
2015-08-24 10:08:48 -0600

    removed console.logs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var utilities = require('../utility/utility.js');
-
 var groupFinal;
-var groupTotal;
-var numGroups;
-var shuffleArray;
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Shuffle Chunk'});
 });
 
 router.post('/submit', function(req, res){
-  groupTotal = req.body.totalNum.split(",");
-  numGroups = req.body.groupNum;
-  shuffleArray = utilities.shuffle(groupTotal);
+  var groupTotal = req.body.totalNum.split(",");
+  var numGroups = req.body.groupNum;
+  var shuffleArray = utilities.shuffle(groupTotal);
   groupFinal = utilities.chunk(shuffleArray, numGroups);
   res.redirect('/chunked');
 });
